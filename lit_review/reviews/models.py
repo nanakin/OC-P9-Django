7e43decbb3758479addb436562d345b7dd271323
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-07-02 23:55:24 +0200

    add the given models.py skeletons

--- lit_review/reviews/models.py
+++ lit_review/reviews/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Ticket(models.Model):
+    pass
+
+
+class Review(models.Model):
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(
+        # validates that rating must be between 0 and 5
+        validators=[MinValueValidator(0), MaxValueValidator(5)])
+    headline = models.CharField(max_length=128)
+    body = models.CharField(max_length=8192, blank=True)
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
--- lit_review/users/models.py
+++ lit_review/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class UserFollows(models.Model):
+    # Your UserFollows model definition goes here
+
+    class Meta:
+        # ensures we don't get multiple UserFollows instances
+        # for unique user-user_followed pairs
+        unique_together = ('user', 'followed_user', )
