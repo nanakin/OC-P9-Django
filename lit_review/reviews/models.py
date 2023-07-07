46f92057a67a41bdf288240a77de579a4966bab1
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-07-07 18:43:07 +0200

    minor adjustements to the models

--- lit_review/reviews/models.py
+++ lit_review/reviews/models.py
@@ -1,11 +1,13 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
 from django.db import models
 
 
-class Ticket(models.Model):
+class Ticket(models.Model):  # name and inheritance fixed by OC
     pass
 
 
-class Review(models.Model):
+class Review(models.Model):  # class fixed by OC
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
     rating = models.PositiveSmallIntegerField(
         # validates that rating must be between 0 and 5
--- lit_review/users/models.py
+++ lit_review/users/models.py
@@ -2,14 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
+class User(AbstractUser):  # name and inheritance fixed by OC
     pass
 
 
-class UserFollows(models.Model):
+"""temporary commented
+class UserFollows(models.Model):  # name and inheritance fixed by OC
     # Your UserFollows model definition goes here
 
     class Meta:
         # ensures we don't get multiple UserFollows instances
         # for unique user-user_followed pairs
-        unique_together = ('user', 'followed_user', )
+        unique_together = ('user', 'followed_user', )  # fixed by OC
+"""
