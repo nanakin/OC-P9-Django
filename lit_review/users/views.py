5fa727ca9d82856d0f5939c44babf63a0f8daa6b
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-07-10 17:59:04 +0200

    add basic url, form, template and view for login functionality

--- lit_review/users/views.py
+++ lit_review/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from . import forms
 
-# Create your views here.
+
+def login_page(request):
+    form = forms.LoginForm()
+    if request.method == "POST":
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            pass
+    return render(request, "users/login.html", context={"form": form})
