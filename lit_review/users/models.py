d430c0a43ca0cbec101bfd51edd3a6403c1d0ef4
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-07-10 17:22:17 +0200

    simplify users database fields

--- lit_review/users/models.py
+++ lit_review/users/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):  # name and inheritance fixed by OC
+    first_name = None
+    last_name = None
+    email = None
     pass
 
 
