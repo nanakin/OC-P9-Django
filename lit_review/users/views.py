708142615f52571ab0b499bdce8bec133af18b89
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-07-17 16:55:35 +0200

    add signup feature

--- lit_review/users/views.py
+++ lit_review/users/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -13,11 +14,24 @@ def login_page(request):
                                 password=form.cleaned_data["password"])
             if user is not None:
                 login(request, user)
-                return redirect('home')
+                return redirect(settings.LOGIN_REDIRECT_URL)
 
     return render(request, "users/login.html", context={"form": form})
 
 
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == "POST":
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        else:
+            print("invalid form")
+    return render(request, "users/signup.html", context={"form": form})
+
+
 def logout_user(request):
     logout(request)
-    return redirect('home')
+    return redirect(settings.LOGOUT_REDIRECT_URL)
