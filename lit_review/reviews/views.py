c0be05abdf44043f92609e9749831a1017bfc83e
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-07-17 20:49:03 +0200

    list user's tickets in the home page

--- lit_review/reviews/views.py
+++ lit_review/reviews/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.db.models import CharField, Value
 from django.contrib.auth.decorators import login_required
+from .models import Ticket
+
+
+def get_users_viewable_tickets(username):
+    tickets = Ticket.objects.filter(user=username)
+    return tickets
 
 
 @login_required()
 def stream_page(request):
-    return render(request, "reviews/stream.html")
+    tickets = get_users_viewable_tickets(request.user)
+    tickets = tickets.annotate(content_type=Value('TICKET', CharField()))
+    posts = sorted(tickets, key=lambda post: post.time_created, reverse=True)
+    return render(request, "reviews/stream.html", context={'posts': posts})
