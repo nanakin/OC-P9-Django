1b8b604df1371e0b5aeba8c2ca73d7bebeecc282
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-07-18 11:22:14 +0200

    add reviews related to user tickets

--- lit_review/reviews/views.py
+++ lit_review/reviews/views.py
@@ -3,6 +3,7 @@ from django.db.models import CharField, Value
 from django.contrib.auth.decorators import login_required
 from itertools import chain
 from .models import Ticket, Review
+from django.db.models import Q
 
 
 def get_users_viewable_tickets(username):
@@ -11,7 +12,7 @@ def get_users_viewable_tickets(username):
 
 
 def get_users_viewable_reviews(username):
-    reviews = Review.objects.filter(user=username)
+    reviews = Review.objects.filter(Q(user=username) | Q(ticket__in=Ticket.objects.filter(user=username)))
     return reviews
 
 
@@ -20,6 +21,6 @@ def stream_page(request):
     tickets = get_users_viewable_tickets(request.user)
     tickets = tickets.annotate(content_type=Value('TICKET', CharField()))
     reviews = get_users_viewable_reviews(request.user)
-    reviews = reviews.annotate(content_type=Value('TICKET', CharField()))
+    reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
     posts = sorted(chain(tickets, reviews), key=lambda post: post.time_created, reverse=True)
     return render(request, "reviews/stream.html", context={'posts': posts})
