3d093616016d55b648f30c405c505a119a3167fe
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-07-19 10:35:52 +0200

    move UserFollows model back to users application

--- lit_review/reviews/admin.py
+++ lit_review/reviews/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Ticket, Review, UserFollows
+from .models import Ticket, Review
 
 admin.site.register(Ticket)
 admin.site.register(Review)
-admin.site.register(UserFollows)
--- lit_review/users/models.py
+++ lit_review/users/models.py
@@ -1,7 +1,21 @@
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.db import models
 
 
 class User(AbstractUser):  # name and inheritance fixed by OC
     first_name = None
     last_name = None
     email = None
+
+
+class UserFollows(models.Model):  # name and inheritance fixed by OC, content suggested in the UML diagram
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                             related_name='following')
+    followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                                      related_name='followed_by')
+
+    class Meta:
+        # ensures we don't get multiple UserFollows instances
+        # for unique user-user_followed pairs
+        unique_together = ('user', 'followed_user', )  # fixed by OC
