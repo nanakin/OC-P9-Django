9813fc1dfbf3e6dea8d683c9c9d9a1dc9b65173d
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-07-27 11:08:29 +0200

    update visual appearance

--- lit_review/reviews/forms.py
+++ lit_review/reviews/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, BooleanField, HiddenInput
+from django.forms import ModelForm, BooleanField, HiddenInput, ChoiceField, RadioSelect
 from .models import Ticket, Review
 
 
@@ -6,6 +6,7 @@ class TicketForm(ModelForm):
     class Meta:
         model = Ticket
         fields = ["title", "description", "image"]
+        labels = {"title": "Titre"}
 
 
 class EditTicketForm(TicketForm):
@@ -20,9 +21,17 @@ class DeleteTicketForm(TicketForm):
 
 
 class ReviewForm(ModelForm):
+
     class Meta:
         model = Review
         fields = ["headline", "rating", "body"]
+        rating_choices = zip([i for i in range(0, 6)], [str(i) for i in range(0, 6)])
+        widgets = {
+            "rating": RadioSelect(choices=rating_choices)
+        }
+        labels = {"headline": "Titre",
+                  "rating": "Note",
+                  "body": "Commentaire"}
 
 
 class EditReviewForm(ReviewForm):
